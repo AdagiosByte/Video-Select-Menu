ef5910e19f0544df3d249b7953b79604dc844881
AdagiosByte <43786133+AdagiosByte@users.noreply.github.com>
2023-03-21 11:26:02 -0400

    Add files via upload
    Fixed Progress Bar.

--- script.js
+++ script.js
@@ -14,23 +14,20 @@ function onSliderClick(slider){
     const shiftd_index = parseInt(getComputedStyle(shiftd).getPropertyValue("--slider_index"))
     if (slider.classList.contains("right-arrow")){
         shiftd.style.setProperty("--slider_index", shiftd_index+1)
-        updateProgressBar(shiftd_index)
+        updateProgressBar(shiftd_index+1)
     }
     if (slider.classList.contains("left-arrow")){
         shiftd.style.setProperty("--slider_index", shiftd_index-1)
-        updateProgressBar(shiftd_index)
+        updateProgressBar(shiftd_index-1)
     }
 }
 
 function updateProgressBar(index){
-    let item1 = document.getElementsByClassName("item1")
-    let item2 = document.getElementsByClassName("item2")
-    let item3 = document.getElementsByClassName("item3")
-    switch(index){
-        case 0:
-            item1.style.backgroundColor = "red"
-            item2.style.backgroundColor = "black"
-            item3.style.backgroundColor = "green"
-            break
+    const bars = document.getElementsByClassName("bar")
+    for(let i=0;i<3;i++){
+        bars[i].className = "bar"
+        if (i === index){
+            bars[i].className = "bar selected-bar"
+        }
     }
 }
